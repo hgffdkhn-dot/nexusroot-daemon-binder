ee991416284085b1b65ed7af286f0164db7ee9ca
hgffdkhn-dot <hgffdkhn@gmail.com>
2026-07-26 10:57:06 +0800

    Refactor NexusRoot service initialization
    Updated service registration and logging for NexusRoot.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include "NexusRootService.h"
-#include <binder/ProcessState.h>
-#include <binder/IPCThreadState.h>
-
-using namespace android;
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <android/log.h>
 
 int main() {
-    // 设置 Binder 线程池
-    ProcessState::self()->setThreadPoolMaxThreadCount(4);
-    // 注册服务
-    NexusRootService::publishAndJoinThreadPool();
+    __android_log_print(ANDROID_LOG_INFO, "nexusrootd", "Starting NexusRoot NDK service...");
+
+    auto service = ndk::SharedRefBase::make<NexusRootService>();
+    binder_status_t status = AServiceManager_addService(service->asBinder().get(), "nexusroot");
+
+    if (status == STATUS_OK) {
+        __android_log_print(ANDROID_LOG_INFO, "nexusrootd", "Service 'nexusroot' registered.");
+    } else {
+        __android_log_print(ANDROID_LOG_ERROR, "nexusrootd", "Failed to register service: %d", status);
+        return 1;
+    }
+
+    // 加入 Binder 线程池
+    ABinderProcess_setThreadPoolMaxThreadCount(4);
+    ABinderProcess_startThreadPool();
+    ABinderProcess_joinThreadPool();
+
     return 0;
 }
